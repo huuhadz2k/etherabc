589d9cf30b1ca49685a079e79ca260b387e3a0be
huuhadz2k <36265491+huuhadz2k@users.noreply.github.com>
2018-03-18 15:22:39 +0700

    No-CMT

---
+++ www/config/environment.js
@@ -0,0 +1,66 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'pool',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'hash',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // API host and port
+      ApiUrl: '//216.244.73.133/',
+
+      // HTTP mining endpoint
+      HttpHost: 'http://216.244.73.133',
+      HttpPort: 8888,
+
+      // Stratum mining endpoint
+      StratumHost: '216.244.73.133',
+      StratumPort: 8008,
+
+      // Fee and payout details
+      PoolFee: '0.5%',
+      PayoutThreshold: '5 YoCoin',
+
+      // For network hashrate (change for your favourite fork)
+      BlockTime: 14.4
+    }
+  };
+
+  if (environment === 'development') {
+    /* Override ApiUrl just for development, while you are customizing
+      frontend markup and css theme on your workstation.
+    */
+    ENV.APP.ApiUrl = 'http://216.244.73.133:8080/'
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
